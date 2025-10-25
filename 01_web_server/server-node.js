217b647c0764b4d089bd8ef9c5f1d73c3694e8d5
dev_nick <developernikhil28@gmail.com>
2025-10-25 22:47:43 +0530

    Added few routes for web-server

--- 01_web_server/server-node.js
+++ 01_web_server/server-node.js
@@ -4,9 +4,19 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello, World!\n');
+    }else if (req.url === '/about') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('About Us Page\n');
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('404 Not Found\n');
+    }
 });
 
 server.listen(port, hostname, () => {
