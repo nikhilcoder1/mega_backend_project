ff200211b701e6f3ecbcdcf72898014fac7d564c
dev_nick <developernikhil28@gmail.com>
2025-10-26 23:27:52 +0530

    Get all teas route

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,14 +1,32 @@
 import express from "express";
 
-const hostname = "127.0.0.1";
 const port = 3000;
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World : Nodemon attached ");
+let teaData = [];
+let nextId = 1;
+
+// add a new tea
+app.post("/tea", (req, res) => {
+  const { name, price } = req.body;
+  const newTea = {
+    id: nextId++,
+    name,
+    price,
+  };
+  teaData.push(newTea);
+  res.status(201).send(newTea);
 });
 
-app.listen(port,() => {
-    console.log(`App listening on http://${hostname}.com : ${port}`)
+// get all teas
+app.get("/teas" , (req,res) => {
+    res.status(201).send(teaData) 
 })
+
+
+
+app.listen(port, () => {
+  console.log(`App listening on: ${port}`);
+});
