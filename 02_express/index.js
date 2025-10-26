3b2711a60b27a67819aa7fe9e9f17bd8ffc384d5
dev_nick <developernikhil28@gmail.com>
2025-10-26 23:28:45 +0530

    Add other routes

--- 02_express/index.js
+++ 02_express/index.js
@@ -25,6 +25,40 @@ app.get("/teas" , (req,res) => {
     res.status(201).send(teaData) 
 })
 
+// get a tea with id
+app.get("/teas/:id", (req, res) => {
+  const tea = teaData.find((tea) => tea.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("Tea not found");
+  }
+
+  res.status(200).send(tea);
+});
+
+// update tea
+app.put("/teas/:id", (req, res) => {
+  const tea = teaData.find((tea) => tea.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("Tea not found");
+  }
+
+  const { name, price } = req.body;
+  tea.name = name;
+  tea.price = price;
+
+  res.status(200).send(tea);
+});
+
+// delete tea
+app.delete("/teas/:id", (req, res) => {
+  const index = teaData.findIndex((tea) => tea.id === parseInt(req.params.id));
+  if (index === -1) {
+    return res.status(404).send("Tea not found");
+  }
+
+  teaData.splice(index, 1);
+  res.status(204).send("Deleted");
+});
 
 
 app.listen(port, () => {
