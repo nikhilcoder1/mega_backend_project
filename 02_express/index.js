c6fcdc49388a3f53ad64df2dbf511d950383c4ba
dev_nick <developernikhil28@gmail.com>
2025-10-27 23:54:27 +0530

    Add middleware and morgan

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 dotenv.config({
   path:'.env'
@@ -10,11 +12,32 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
+const morganFormat = ":method :url :status :response-time ms";
+
+// middleware
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const parts = message.trim().split(' ');
+        const logObject = {
+          method: parts[0],
+          url: parts[1],
+          status: parts[2],
+          responseTime: parts[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 let teaData = [];
 let nextId = 1;
 
 // add a new tea
 app.post("/tea", (req, res) => {
+  logger.info("A post request was made to add tea")
   const { name, price } = req.body;
   const newTea = {
     id: nextId++,
